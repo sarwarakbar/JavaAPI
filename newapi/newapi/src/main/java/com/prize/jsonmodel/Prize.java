e6e12b9df506f7945b74d867dda348225ff1d5c1
sarwarakbar <sarwar.bly@gmail.com>
2022-08-16 15:54:25 +0530

    update

--- newapi/newapi/src/main/java/com/prize/jsonmodel/Prize.java
+++ newapi/newapi/src/main/java/com/prize/jsonmodel/Prize.java
@@ -1,9 +1,6 @@
 package com.prize.jsonmodel;
-
-
 import java.util.List;
-
-
+import com.prize.models.Laureate;
 
 public class Prize 
 {
@@ -11,4 +8,41 @@ public class Prize
 	    public String category;
 	    public List<Laureate> laureates;
 	    public String overallMotivation;
+	   
+	     	     
+	    public String getYear() { return year;}
+	    public void setYear(String year) {this.year = year;}
+	   	
+	    public String getCategory() {return category;}
+	    public void setCategory(String category) {this.category = category;}
+	     
+	    public List<Laureate> getList(){return laureates;}
+	    public void setList(List<Laureate> laureates) {this.laureates = laureates;}
+	    
+	    public String getOverallMotivation() {return overallMotivation; }
+	    public void setOverallMotivation(String overallMotivation) { this.overallMotivation = overallMotivation;}  
+	    
 }
+	    
+	    
+	   	
+	    
+	    
+	     
+	    
+	   	
+	    
+	   			     
+	     
+	    
+	   	
+	    
+	    
+	    
+	       
+	   	     
+	    
+	    
+	    
+	   	
+
--- newapi/newapi/src/main/java/com/prize/models/Prize.java
+++ newapi/newapi/src/main/java/com/prize/models/Prize.java
@@ -1,12 +1,9 @@
 package com.prize.models;
-
 import java.util.List;
-
 import javax.persistence.Column;
 import javax.persistence.Entity;
 import javax.persistence.Id;
 import javax.persistence.Table;
-
 @Entity
 @Table
 public class Prize 
@@ -23,55 +20,51 @@ public class Prize
 	    @Column
 	    public String overallMotivation;
 	    
- public int getId() 
- {
-	 return PrizeId;	    
- }
+ public int getId() {return PrizeId;}  
+ public void setId(int PrizeId) {this.PrizeId = PrizeId;}
  
- public void setId(int PrizeId) 
- {
-	this.PrizeId = PrizeId;
- }
-	     
- public String getYear() 
- {
-	return year;
- }
+ public String getYear() {return year;}
+ public void setYear(String year) {this.year = year;}
+	
+ public String getCategory() {return category;}
+ public void setCategory(String category) {	this.category = category;}     
+  
+ public List<Laureate> getList(){ return laureates;}
+ public void setList(List<Laureate> laureates) {this.laureates = laureates;}	
  
- public void setYear(String year) 
- {
-	this.year = year;
- }
+ public String getOverallMotivation() { return overallMotivation;}
+ public void setOverallMotivation(String overallMotivation) {this.overallMotivation = overallMotivation;}
+}
+  
+
+	
+ 
 		     
- public String getCategory() 
- {
-	return category;
- }
  
- public void setCategory(String category) 
- {
-	this.category = category;
- }
+ 
+	
+ 
+ 
+ 
+ 
+	
+ 
 			     
- public List<Laureate> getList() 
- {
-	return laureates;
- }
+
  
- public void setList(List<Laureate> laureates)
- {
-    this.laureates = laureates;
- }
+	
+ 
+ 
+ 
+ 
+    
+ 
 	     
- public String getOverallMotivation() 
- {
-	return overallMotivation;
- }
  
- public void setOverallMotivation(String overallMotivation) 
- {
-	this.overallMotivation = overallMotivation;
- }
+	
+ 
+ 
+
 	    
 
-}
+
